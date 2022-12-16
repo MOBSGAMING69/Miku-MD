4b365eed0193a95507b7850ffc4000579d308514
MOBSGAMING69 <119100597+MOBSGAMING69@users.noreply.github.com>
2022-12-16 02:12:19 -0800

    Update config.js

--- config.js
+++ config.js
@@ -7,20 +7,20 @@ global.autoReadAll = false
 global.antitags = true
 
 
-global.Owner = ['918101187835']
-global.OwnerNumber = ['918101187835']
-global.ownertag = ['918101187835']
+global.Owner = ['971563880249']
+global.OwnerNumber = ['971563880249']
+global.ownertag = ['971563880249']
 global.BotName = "Miku"
 global.packname = "Miku bot"
 global.author = "By: Fantox"
-global.OwnerName = "Fantox"
+global.OwnerName = "MOBS"
 global.BotSourceCode = "https://github.com/FantoX001/Miku-MD"
 global.SupportGroupLink = "https://chat.whatsapp.com/JcBzjRIxM5UHfuJbt0KrWt"
 global.sessionName = "session"
 
 
 
-global.prefa = ['-'] // Prefix ---  change it to anythong you want except @//
+global.prefa = ['/'] // Prefix ---  change it to anythong you want except @//
 
 
 global.location = "West Bengal, India"
